fa114e1e78bb5bb8c547f3874a8a214938e4c19e
ValeriiaYefymova <151965857+ValeriiaYefymova@users.noreply.github.com>
2024-03-10 10:08:43 +0100

    hw3

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function filterArray(numbers, value) {
+  let filteredArray = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
+    }
+  }
+
+  return filteredArray;
+}
